7c9b5a07c37ca7f6f76db2afecc63780ac04c29d
geoni153 <63406193+geoni153@users.noreply.github.com>
2021-02-01 19:54:04 +0900

    creating mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,49 @@
-export const people = [
+const movies = [
     {
-        id: "0",
-        name: "geoni",
-        age: 18,
-        gender: "female"
+        id: 0,
+        name: "Love&Trouble",
+        score: 3
     },
     {
-        id: "1",
-        name: "duck",
-        age: 20,
-        gender: "female"
-     },
-     {
-        id: "2",
-        name: "han",
-        age: 30,
-        gender: "male"
+        id: 1,
+        name: "Old Boy",
+        score: 4
     },
     {
-        id: "3",
-        name: "ddong",
-        age: 31,
-        gender: "male"
+        id: 0,
+        name: "A Star Is Born",
+        score: 5
+    },
+    {
+        id: 0,
+        name: "CoCo",
+        score: 5
     }
  ];
 
+ export const getMovies = () => movies;
+
  export const getById = id => {
-     const filterePeople = people.filter(person => person.id === String(id));
-     return filterePeople[0];
- }
+     const filteredMovies = movies.filter(movie => movie.id === String(id));
+     return filteredMovies[0];
+ };
+
+ export const deleteMovie = (id) => {
+     const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+     if (movies.length > cleanedMovies.length) {
+         movies = cleanedMovies;
+         return true;
+     } else {
+         return false;
+     }
+ };
+
+ export const addMovie = (name, score) => {
+     const newMovie = {
+         id: `${movies.length + 1}`,
+         name,
+         score
+     };
+     movies.push(newMovie);
+     return newMovie;
+ };
