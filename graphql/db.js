1ac0cc4d9a274f119ba38a164bf35d7a1285cf2f
geoni153 <63406193+geoni153@users.noreply.github.com>
2021-02-01 20:51:40 +0900

    wrapping REST API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,21 @@
-let movies = [
-    {
-        id: 0,
-        name: "Love&Trouble",
-        score: 3
-    },
-    {
-        id: 1,
-        name: "Old Boy",
-        score: 4
-    },
-    {
-        id: 2,
-        name: "A Star Is Born",
-        score: 5
-    },
-    {
-        id: 3,
-        name: "CoCo",
-        score: 5
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
+
+// export const getMovies = (limit, rating) => {
+//     return  fetch(`${API_URL}`)
+//     .then(res => res.json())
+//     .then(json => json.data.movies);
+// };
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if (limit > 0) {
+        REQUEST_URL +=  `limit=${limit}`;
     }
- ];
-
- export const getMovies = () => movies;
-
- export const getById = id => {
-     const filteredMovies = movies.filter(movie => movie.id === id);
-     return filteredMovies[0];
- };
-
- export const deleteMovie = (id) => {
-     const cleanedMovies = movies.filter(movie => movie.id !== id);
-     if (movies.length > cleanedMovies.length) {
-         movies = cleanedMovies;
-         return true;
-     } else {
-         return false;
-     }
- };
-
- export const addMovie = (name, score) => {
-     const newMovie = {
-         id: `${movies.length + 1}`,
-         name,
-         score
-     };
-     movies.push(newMovie);
-     return newMovie;
- };
+    if (rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+    return fetch(REQUEST_URL)
+    .then(res => res.json())
+    .then(json => json.data.movies);
+};
